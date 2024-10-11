a211af7dc532256d1d777f24c600bad1320348b0
HuyDangGigarion <huy.dang@gigarion.com>
2024-10-11 16:29:27 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,11 @@ module.exports = defineConfig({
     baseUrl: 'http://uitestingplayground.com',
     excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    reporter: 'cypress-junit', // Specify the reporter here
+    reporterOptions: {
+      mochaFile: 'cypress/results/junit-[hash].xml', // Path for JUnit report
+      toConsole: true, // Optional: log output to console
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
