465e4b9ed23aa513029b59876db568f4235646c2
huydh <danghoanghuy.0910@gmail.com>
2024-10-14 15:58:11 +0700

    -update fie config cypress

--- cypress.config.js
+++ cypress.config.js
@@ -8,11 +8,19 @@ module.exports = defineConfig({
     baseUrl: 'http://uitestingplayground.com',
     excludeSpecPattern: ['**/1-getting-started', '**/2-advanced-examples'],
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    reporter: 'cypress-junit', // Specify the reporter here
-    reporterOptions: {
-      mochaFile: 'cypress/results/junit-[hash].xml', // Path for JUnit report
-      toConsole: true, // Optional: log output to console
-    },
+    reporter: 'cypress-mochawesome-reporter', // Specify the reporter here
+      reporterOptions: {
+        charts: true,
+        overwrite: false,
+        html: true,
+        json: true
+    }
+    // reporter: 'cypress-junit', // Specify the reporter here
+    // reporterOptions: {
+    //   mochaFile: 'cypress/results/junit-[hash].xml', // Path for JUnit report
+    //   toConsole: true, // Optional: log output to console
+    // }
+    ,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
